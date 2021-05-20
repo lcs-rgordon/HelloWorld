ac5b192744078a25d08dd6695d8cd71b17dfe50e
Russell Gordon <rgordon@lcs.on.ca>
2021-05-20 09:33:59 -0400

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Russell Gordon on 2021-05-20.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
